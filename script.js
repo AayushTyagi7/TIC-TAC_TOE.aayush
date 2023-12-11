d6bd02a8b5ce68f217833fb8ce4d5fa960bf1d8f
Aayush Tyagi <104324109+AayushTyagi7@users.noreply.github.com>
2023-12-12 02:24:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,108 @@
-// Tic Tac Toe Game Logic
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+//Winning Pattern Array
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+//Player 'X' plays first
+let xTurn = true;
+let count = 0;
 
-var board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
-  
-  var currentPlayer = 'X';
-  var moves = 0;
-  var gameOver = false;
-  
-  function makeMove(row, col) {
-    if (gameOver || board[row][col] !== '') {
-      return;
-    }
-  
-    board[row][col] = currentPlayer;
-    document.getElementById('board').children[row].children[col].innerText = currentPlayer;
-    moves++;
-  
-    if (checkWin(row, col)) {
-      document.getElementById('message').innerText = currentPlayer + ' wins!';
-      gameOver = true;
-    } else if (moves === 9) {
-      document.getElementById('message').innerText = 'It\'s a draw!';
-      gameOver = true;
-    }
-  
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+//Disable All Buttons
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+  //enable popup
+  popupRef.classList.remove("hide");
+};
+
+//Enable all buttons (For New Game and Restart)
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+  //disable popup
+  popupRef.classList.add("hide");
+};
+
+//This function is executed when a player wins
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+  } else {
+    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
   }
-  
-  function checkWin(row, col) {
-    var symbol = board[row][col];
-  
-    // Check row
-    if (board[row][0] === symbol && board[row][1] === symbol && board[row][2] === symbol) {
-      return true;
-    }
-  
-    // Check column
-    if (board[0][col] === symbol && board[1][col] === symbol && board[2][col] === symbol) {
-      return true;
-    }
-  
-    // Check diagonals
-    if (board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol) {
-      return true;
-    }
-  
-    if (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol) {
-      return true;
-    }
-  
-    return false;
-  }
-  
-  function resetBoard() {
-    board = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
+};
+
+//Function for draw
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+};
+
+//New Game
+newgameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+
+//Win Logic
+const winChecker = () => {
+  //Loop through all win patterns
+  for (let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
     ];
-    currentPlayer = 'X';
-    moves = 0;
-    gameOver = false;
-  
-    var cells = document.getElementsByClassName('cell');
-    for (var i = 0; i < cells.length; i++) {
-      cells[i].innerText = '';
+    //Check if elements are filled
+    //If 3 empty elements are same and would give win as would
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+      if (element1 == element2 && element2 == element3) {
+        //If all 3 buttons have same values then pass the value to winFunction
+        winFunction(element1);
+      }
     }
-  
-    document.getElementById('message').innerText = '';
   }
-  
-  
+};
+
+//Display X/O on click
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+      //Display X
+      element.innerText = "X";
+      element.disabled = true;
+    } else {
+      xTurn = true;
+      //Display Y
+      element.innerText = "O";
+      element.disabled = true;
+    }
+    //Increment count on each click
+    count += 1;
+    if (count == 9) {
+      drawFunction();
+    }
+    //Check for win on every click
+    winChecker();
+  });
+});
+//Enable Buttons and disable popup on page load
+window.onload = enableButtons;
